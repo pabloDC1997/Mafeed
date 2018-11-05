c78382e95189889a81431c9d50bf7ef9584ec2d5
Pablo <akpablo1997@gmail.com>
2018-11-05 18:15:38 -0200

    first commit

--- app/src/main/java/br/com/app/mafeed/services/RetrofitClient.java
+++ app/src/main/java/br/com/app/mafeed/services/RetrofitClient.java
@@ -1,6 +1,9 @@
 package br.com.app.mafeed.services;
 
+import android.content.Context;
 import android.util.Log;
+
+import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -9,7 +12,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
  * Created by Pablo on 17/05/2017.
  */
 
-public class RetrofitClient {
+public abstract class RetrofitClient {
 
     public static Retrofit getClient(String baseUrl) {
      Retrofit retrofit = new Retrofit.Builder()
@@ -17,6 +20,20 @@ public class RetrofitClient {
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
         Log.i("INFO: ", "Initialized retrofit instance on base: " + retrofit.baseUrl());
+        return retrofit;
+    }
+
+    private Retrofit getClient(String baseUrl) {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .addConverterFactory(GsonConverterFactory.create())
+//                    .addConverterFactory(SimpleXmlConverterFactory.createNonStrict(
+//                            new Persister(new AnnotationStrategy() // important part!
+//                            )
+                    .baseUrl(baseUrl)
+                    .build();
+        }
         return retrofit;
     }
 }
